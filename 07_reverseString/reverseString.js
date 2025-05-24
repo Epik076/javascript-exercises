c6fc98b31009d027149e6defc15e884ecfb63faa
Epik076 <209223850+Epik076@users.noreply.github.com>
2025-05-24 23:08:52 +0200

    Done till Task 07, in 07 the solution shows sophisticated Lambda like code. My solution is still a simple easy readable function...

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	let result = "";
+	let char = '';
+	for(let i = string.length; i >= 0; i--) {
+		char = string.charAt(i);
+		result += char;
+	}
+	return result;
 };
 
 // Do not edit below this line

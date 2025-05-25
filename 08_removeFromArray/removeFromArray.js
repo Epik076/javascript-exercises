6a263cd9669854888c2a0fa66e7c993df400ec72
Epik076 <209223850+Epik076@users.noreply.github.com>
2025-05-25 23:05:19 +0200

    solved two more excercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,31 @@
+/*
+// Old school way using the arguments object
+
 const removeFromArray = function() {
+	const arr = arguments[0];
+	const valuesToRemove = Array.from(arguments).slice(1);
+	const newArr = [];
+	for(let i = 0; i < arr.length; i++) {
+		if(!valuesToRemove.includes(arr[i])) {
+			newArr.push(arr[i]);	
+		}
+	}
+	return newArr;
 };
+*/
+
+// Modern way of using Rest parameters ...
+
+const removeFromArray = function(arr, ...args) {
+	const newArr = [];
+
+	for(let i = 0; i < arr.length; i++) {
+		if(!args.includes(arr[i])) {
+			newArr.push(arr[i]);
+		}
+	}
+	return newArr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(startInt, endInt) {
+	let total = 0;
+	let cache = 0;
+	if( startInt < 0 || endInt < 0 || (startInt % 1 !== 0) || (endInt % 1 !== 0) || Number(startInt) !== startInt || Number(endInt) !== endInt ) 
+	{
+	return 'ERROR';
+	}
+	if(startInt > endInt) 
+	{
+	cache = startInt;
+	startInt = endInt;
+	endInt = cache;
+	}
+	for(let i = startInt; i <= endInt; i++) {
+		total += i;
+	}
+	return total;
 };
 
 // Do not edit below this line

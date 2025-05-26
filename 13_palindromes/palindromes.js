ac0fd62515e7e6e0b7fd15bec8c28a8b3e3fadb6
Epik076 <209223850+Epik076@users.noreply.github.com>
2025-05-26 20:55:42 +0200

    Finished Task 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	string = string.replace(/[^\p{L}\p{N}]/gu, '');
+	string = string.toLowerCase();
+	let strArr = string.split('');
+	let reverseArray = [];
+	let strLength = string.length - 1;
+	for(let i = 0; i < strArr.length; i++) 
+	{
+		reverseArray[i] = strArr[strLength];
+		strLength--;
+	}
+	for(let i = 0; i < strArr.length; i++) 
+	{
+		if(reverseArray[i] !== strArr[i]) 
+		{
+			return false;
+		}
+	}
+	return true;
 };
 
 // Do not edit below this line

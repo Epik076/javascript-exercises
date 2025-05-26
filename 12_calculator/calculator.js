92ffcd3df08c207a495bc718b4e511ecef85bc15
Epik076 <209223850+Epik076@users.noreply.github.com>
2025-05-26 15:37:25 +0200

    Finished Task 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+	for(let i = 0; i < arr.length; i++) 
+	{
+		sum += arr[i];
+	}
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	let product = 1;
+	for(let i = 0; i < arr.length; i++) 
+	{
+		product *= arr[i];
+	}
+	return product;
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+	let result = base;
+	for(let i = 1; i < exponent; i++) 
+	{
+		result *= base;
+	}
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if( num === 0 || num === 1) 
+	{
+		return 1;
+	}
+	let i = num - 1;
+	while(i > 0) 
+	{
+		num *= i;
+		i--;
+	}
+	return num;
 };
 
 // Do not edit below this line

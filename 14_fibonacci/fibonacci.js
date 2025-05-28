a775cd945a45ae337d1eb6f397fca3db6ed8778f
Epik076 <209223850+Epik076@users.noreply.github.com>
2025-05-28 15:23:31 +0200

    Finished Task 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+	Number(num);
+	if(num < 0){
+	return "OOPS";
+	}
+	let x = 0;
+	let y = 1;
+	for(let i = 0; i < num; i++) {
+		let z = x + y;
+		x = y;
+		y = z;
+	}
+	return x;
 };
 
 // Do not edit below this line
